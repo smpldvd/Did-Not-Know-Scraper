ced4523a246b714b065cec75e5cb8be8f2802008
David Luu <David.n.luu@gmail.com>
2018-08-31 18:16:53 -0500

    Setting temp HTML for page layout

--- server.js
+++ server.js
@@ -1,49 +1,51 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const mongoose = require("mongoose");
+const express = require('express')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const mongoose = require('mongoose')
 
-const cheerio = require("cheerio");
+// Scrape
+const request = require('request')
+const cheerio = require('cheerio')
 
 // Require all models
-let db = require("./models");
+let db = require('./models')
 
-let PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000
 
 // Init Express
-const app = express();
+const app = express()
 
 // Configure middleware
 
 // Use morgan logger for logging requests
-app.use(logger("dev"));
+app.use(logger('dev'))
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 // Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
+app.use(express.static('public'))
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+var exphbs = require('express-handlebars')
 
-app.engine("hbs", exphbs({ defaultLayout: "main", extname: '.hbs' }));
-app.set("view engine", ".hbs");
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines'
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI);
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URI)
 
-// Start the server
-mongoose.connection.on('error', function(err) {
-    console.log("Mongoose Error: " + err);
+// Catch mongoose errors
+mongoose.connection.on('error', err => {
+  console.log('Mongoose Error: ' + err)
 })
 
-mongoose.connection.on('open', function() {
-    console.log("Mongoose connection successful.");
-    app.listen(PORT, function() {
-        console.log("App running on port " + PORT + "!");
-    });
-});
+mongoose.connection.once('open', function () {
+  console.log('Mongoose connection successful.')
+  app.listen(PORT, function () {
+    console.log('App running on port ' + PORT + '!')
+  })
+})
